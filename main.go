25f65da7b47c7ce6150974158c3110834e569d15
devPryze <msaaddev@gmail.com>
2022-08-17 17:13:09 +0500

    created CRUD

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 	"log"
 	"time"
 
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -19,10 +20,10 @@ type MongoInstance struct {
 var mg MongoInstance
 
 const dbName = "fiber-hrm"
-const mongoURI = "mongodb://localhost:27017" + dbName
+const mongoURI = "mongodb://localhost:27017/" + dbName
 
 type Employee struct {
-	ID     int     `json:"id,omitempty" bson:"_id,omitempty"`
+	ID     string  `json:"id,omitempty" bson:"_id,omitempty"`
 	Name   string  `json:"name"`
 	Salary float64 `json:"salary"`
 	Age    int     `json:"age"`
@@ -33,7 +34,7 @@ func Connect() error {
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
-	err = client.connect(ctx)
+	err = client.Connect(ctx)
 	db := client.Database(dbName)
 	if err != nil {
 		return err
@@ -56,23 +57,115 @@ func main() {
 
 	app.Get("/employee", func(c *fiber.Ctx) error {
 
-		query := bson.D{}
+		query := bson.D{{}}
 
-		cur, err := mg.DB.Collection("employees").Find(c.Context(), query)
+		cursor, err := mg.DB.Collection("employees").Find(c.Context(), query)
 		if err != nil {
-			return err.Status(500).SendString(err.Error())
+			return c.Status(500).SendString(err.Error())
 		}
+
 		var employees []Employee = make([]Employee, 0)
 
-		if err := cur.All(c.Context(), &employees); err != nil {
-			return err.Status(500).SendString(err.Error())
+		if err := cursor.All(c.Context(), &employees); err != nil {
+			return c.Status(500).SendString(err.Error())
 		}
+
 		return c.JSON(employees)
+	})
+
+	app.Post("/employee", func(c *fiber.Ctx) error {
+
+		collection := mg.DB.Collection("employees")
+
+		employee := new(Employee)
+
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		employee.ID = ""
+
+		insertionResult, err := collection.InsertOne(c.Context(), employee)
+		if err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		filter := bson.D{{Key: "_id", Value: insertionResult.InsertedID}}
+
+		createdRecord := collection.FindOne(c.Context(), filter)
+
+		createdEmployee := &Employee{}
+
+		createdRecord.Decode(createdEmployee)
+
+		return c.Status(200).JSON(createdEmployee)
 
 	})
 
-	app.Post("/employee")
-	app.Put("/employee/:id")
-	app.Delete("/employee:id")
+	app.Put("/employee/:id", func(c *fiber.Ctx) error {
+
+		idParam := c.Params("id")
+
+		employeeID, err := primitive.ObjectIDFromHex(idParam)
+
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		employee := new(Employee)
+
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		query := bson.D{{Key: "_id", Value: employeeID}}
+
+		update := bson.D{
+			{
+				Key: "$set",
+				Value: bson.D{
+					{Key: "name", Value: employee.Name},
+					{Key: "age", Value: employee.Age},
+					{Key: "salary", Value: employee.Salary},
+				},
+			},
+		}
+
+		err = mg.DB.Collection("employees").FindOneAndUpdate(c.Context(), query, update).Err()
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				return c.SendStatus(400)
+			}
+			return c.SendStatus(500)
+		}
+
+		employee.ID = idParam
+
+		return c.Status(200).JSON(employee)
+	})
+
+	app.Delete("/employee/:id", func(c *fiber.Ctx) error {
+		employeeID, err := primitive.ObjectIDFromHex(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		query := bson.D{{Key: "_id", Value: employeeID}}
+
+		result, err := mg.DB.Collection("employees").DeleteOne(c.Context(), query)
+
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		if result.DeletedCount < 1 {
+			c.SendStatus(404)
+		}
+
+		return c.Status(200).JSON("record deleted")
+
+	})
+
+	log.Fatal(app.Listen(":3000"))
 
 }
